0336b5c007fcac0293c194400d6ce479c7185785
AustinShiflet1 <ashiflet@byupathway.edu>
2025-07-10 17:31:12 -0600

    Add Job and Resume classes with functionality to display job details

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Front End Developer";
+        job1._company = "Google";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "T-Mobile";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Malia De La Rosa";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
