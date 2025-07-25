049b1b7353f6cc5b216b391af20a9af7e963a083
AustinShiflet1 <ashiflet@byupathway.edu>
2025-07-24 19:12:45 -0600

    updated

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -2,7 +2,7 @@ public class Video
 {
     public string Title { get; set; }
     public string Author { get; set; }
-    public int Length { get; set; } // in seconds
+    public int Length { get; set; }
 
     private List<Comment> _comments = new List<Comment>();
 
