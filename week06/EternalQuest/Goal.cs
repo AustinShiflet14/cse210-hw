6707a60725f91b8b3bbf48caba7b0bf6f667a14c
AustinShiflet1 <ashiflet@byupathway.edu>
2025-08-05 18:47:46 -0600

    started on eternal quest assingment

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,16 @@
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string shortName, string description, int points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    
+
+}
